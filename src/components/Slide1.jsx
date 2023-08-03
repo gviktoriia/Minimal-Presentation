2a850a40f5753b895ce1a5c198567bd6b0864fea
Viktoriia Hvozdak <2835638h@student.gla.ac.uk>
2023-08-03 17:11:01 +0300

    First page done

---
+++ src/components/Slide1.jsx
@@ -0,0 +1,81 @@
+import { Box, Grid, Typography } from "@mui/material";
+import RemoveIcon from "@mui/icons-material/Remove";
+import React from "react";
+
+const imageURL =
+  "https://images.unsplash.com/photo-1497170675097-81fc06e915da?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=928&q=80";
+
+function Slide1() {
+  return (
+    <Grid
+      container
+      direction="row"
+      alignItems="center"
+      spacing={1}
+      marginTop="10vh"
+    >
+      <Grid item height="50px" xs={1} textAlign="center">
+        <RemoveIcon
+          sx={{
+            rotate: "90deg",
+            fontSize: "50px",
+            color: "#1C1F25",
+          }}
+        />
+      </Grid>
+      <Grid item xs={10}>
+        <Box
+          height="600px"
+          maxWidth="1440px"
+          sx={{
+            backgroundImage: `url(${imageURL})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundColor: "rgb(28, 31, 37, 0.6)",
+            backgroundBlendMode: "multiply",
+          }}
+        >
+          <Typography
+            sx={{
+              color: "#E3E6EC",
+              fontFamily: "Quicksand",
+              fontSize: "25px",
+              fontWeight: "400px",
+              paddingLeft: "240px",
+              paddingTop: "136px",
+            }}
+          >
+            Multipurpose Minimal Solution
+          </Typography>
+          <Typography
+            sx={{
+              color: "#FFFFFF",
+              fontFamily: "Quicksand",
+              fontSize: "60px",
+              fontWeight: "700px",
+              maxWidth: "467px",
+              textAlign: "left",
+              paddingLeft: "240px",
+            }}
+          >
+            The simplest things are often the truest…
+          </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={1} textAlign="center">
+        <Typography
+          sx={{
+            fontFamily: "Quicksand",
+            fontSize: "60px",
+            fontWeight: "bold",
+            color: "#E3E6EC",
+          }}
+        >
+          01
+        </Typography>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Slide1;
